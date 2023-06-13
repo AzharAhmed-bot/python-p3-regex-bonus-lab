5fc322e40d41a960312f8d2ac4203b0a975e2204
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-06-13 16:10:55 +0300

    complete assignment

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,8 @@
 import re
 
-my_pattern = r""
-my_regex = re.compile(my_pattern)
+
+my_regex = re.compile(r"Some weather we're having today, huh\?|It's such a lovely day today.|Maybe today's just not my day.")
+text="Some weather we're having today, huh?"
+found=re.findall(my_regex,text)
+print(found)
 
